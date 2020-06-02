398ed06a4fbf28942d94bba89e7d070f07de83b4
Edison Lamar <edilata@msn.com>
2020-06-02 13:49:28 +0200

    Register bug fixed with setItem method

--- src/app/auth/sign-up/sign-up.component.ts
+++ src/app/auth/sign-up/sign-up.component.ts
@@ -2,24 +2,35 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
 import { AuthService  } from '../auth.service';
-import { User  } from '../user';
+
+declare var $:any;
+
 
 @Component({
   selector: 'app-sign-up',
   templateUrl: './sign-up.component.html',
   styleUrls: ['./sign-up.component.scss']
 })
+
 export class SignUpComponent implements OnInit {
 
-  constructor(private authService: AuthService, private router: Router) { }
+  constructor(private authService: AuthService, private router: Router) {
+
+  }
 
   ngOnInit(): void {
+    debugger
   }
 
   register(form) {
+    debugger
     console.log(form.value);
     this.authService.register(form.value).subscribe((res) => {
-      this.router.navigateByUrl('home');
+      debugger
+      console.log("Registered!");
+      this.router.navigateByUrl('/welcome');
+      // TODO use angular boostrap version
+      $('#signupModal').modal('hide');
     });
   }
 
